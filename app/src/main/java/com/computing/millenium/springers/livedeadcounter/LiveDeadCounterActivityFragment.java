632c20336897a171205b3eba725e7fc9570d0fbd
Mike <mikespringer49@gmail.com>
2015-06-17 22:31:06 -0400

    Added radio button functionality to quadrant buttons with visual change on checked

--- app/src/main/java/com/computing/millenium/springers/livedeadcounter/LiveDeadCounterActivityFragment.java
+++ app/src/main/java/com/computing/millenium/springers/livedeadcounter/LiveDeadCounterActivityFragment.java
@@ -4,14 +4,18 @@ import android.content.Context;
 import android.os.Vibrator;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.zip.Inflater;
 
 public class LiveDeadCounterActivityFragment extends Fragment {
 
@@ -21,6 +25,8 @@ public class LiveDeadCounterActivityFragment extends Fragment {
 
     private float mViableCellDensity;
     private float mViability;
+
+    private final String TAG = "LiveDeadCounterFragment";
 
     public LiveDeadCounterActivityFragment() {
     }
@@ -39,26 +45,30 @@ public class LiveDeadCounterActivityFragment extends Fragment {
 
         final Vibrator vibrator = (Vibrator)getActivity().getSystemService(Context.VIBRATOR_SERVICE);
 
+        //Get Textviews associated with counts and set to initial count
         final TextView liveCountTextView = (TextView)v.findViewById(R.id.liveTextView);
         final TextView deadCountTextView = (TextView)v.findViewById(R.id.deadTextView);
         liveCountTextView.setText(Integer.toString(mQ1LiveCount));
         deadCountTextView.setText(Integer.toString(mQ1DeadCount));
 
+        //Set calculations results text view and update to current counts
         final TextView calculationTextView = (TextView)v.findViewById(R.id.calculation_text_view);
         calculateAndUpdate(calculationTextView);
 
+        //Set live counter button with haptic feedback and calculation update on each click
         Button liveCounter = (Button)v.findViewById(R.id.liveButton);
         liveCounter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 vibrator.vibrate(20);
                 if (mQ1LiveCount == 999) mQ1LiveCount = 0;
-                else mQ1LiveCount ++;
+                else mQ1LiveCount++;
                 liveCountTextView.setText(Integer.toString(mQ1LiveCount));
                 calculateAndUpdate(calculationTextView);
             }
         });
 
+        //Set dead counter button with haptic feedback and calculation update on each click
         Button deadCounter = (Button)v.findViewById(R.id.deadButton);
         deadCounter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,6 +80,20 @@ public class LiveDeadCounterActivityFragment extends Fragment {
                 calculateAndUpdate(calculationTextView);
             }
         });
+
+        View.OnClickListener quadClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vibrator.vibrate(20);
+            }
+        };
+
+        RadioGroup quadrantGroup = new RadioGroup(getActivity());
+        //TODO: Add all buttons to a radiogroup
+        RadioButton Q1Button = (RadioButton)v.findViewById(R.id.quadrant_one_button);
+        RadioButton Q2Button = (RadioButton)v.findViewById(R.id.quadrant_two_button);
+        RadioButton Q3Button = (RadioButton)v.findViewById(R.id.quadrant_three_button);
+        RadioButton Q4Button = (RadioButton)v.findViewById(R.id.quadrant_four_button);
 
         return v;
     }
